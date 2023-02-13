9bcfe5fd7b160dbb748b037e0a011c31439d80be
Enis Gjini <gjinienis148@gmail.com>
2023-02-13 20:45:32 +0100

    Ndryshimi i fjalekalimit tek pjesa e konektimit

--- conn-d.php
+++ conn-d.php
@@ -1,9 +1,9 @@
 <?php
-$conn = new mysqli("localhost","root","","bareshao_f");
+$conn = new mysqli("198.38.83.75","bareshao_f","sk9_b64N4","bareshao_f");
 
 // Check connection
 if ($conn -> connect_errno) {
   echo "Failed to connect to MySQL: " . $conn -> connect_error;
   exit();
 }
-
+?>
