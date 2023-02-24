ae5ef727ee1c1d57f37459a2148e7d74a54035e1
Enis Gjini <egjini17@gmail.com>
2023-02-24 20:26:49 +0100

    #

--- conn-d.php
+++ conn-d.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$conn = new mysqli("198.38.83.75","bareshao_f","6D2?19slm","bareshao_f");
+$conn = new mysqli("198.38.83.75", "bareshao_f", "6D2?19slm", "bareshao_f");
 // $conn = new mysqli("localhost", "root", "", "bareshao_f");
 
 // Check connection
